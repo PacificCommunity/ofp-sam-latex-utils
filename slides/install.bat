d8697e86125bd1ce7ac1f1d02a96565aaa592145
Arni Magnusson <thisisarni@gmail.com>
2022-01-27 14:15:50 +1100

    Overwrite existing files

--- slides/install.bat
+++ slides/install.bat
@@ -1,3 +1,8 @@
+@echo off
+
 md c:\home\latex\sty\spc
-xcopy sty c:\home\latex\sty\spc /s
+xcopy sty c:\home\latex\sty\spc /s /y
 setx TEXINPUTS .\\;c:\home\latex\\;
+
+echo Installation completed
+pause
